9ad94a0d1241293072ba9272c6d725ba2138e60c
Soufyan <elomranisoufyan@gmail.com>
2026-03-30 17:09:56 +0100

    finished client side basic registration now the client can sign up using backend register api endpoint

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import register from '@/views/Auth/register.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -17,6 +17,7 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    { path: '/register', component: register },
   ],
 })
 
---
+++ src/views/Auth/register.vue
@@ -0,0 +1,40 @@
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+const name = ref('')
+const email = ref('')
+const password = ref('')
+const response = ref('')
+async function register() {
+  try {
+    response.value = await axios.post('http://localhost/api/register', {
+      name: name.value,
+      email: email.value,
+      password: password.value,
+    })
+    console.log('Registration successful:', response.value.data)
+  } catch (error) {
+    console.error('Registration error:', error)
+  }
+}
+</script>
+<template>
+  <form @submit.prevent="register" class="needs-validation" novalidate>
+    <div class="mb-3">
+      <label for="name" class="form-label">Name:</label>
+      <input id="name" v-model="name" type="text" class="form-control" required />
+      <div class="invalid-feedback">Please provide a valid name.</div>
+    </div>
+    <div class="mb-3">
+      <label for="email" class="form-label">Email:</label>
+      <input id="email" v-model="email" type="email" class="form-control" required />
+      <div class="invalid-feedback">Please provide a valid email.</div>
+    </div>
+    <div class="mb-3">
+      <label for="password" class="form-label">Password:</label>
+      <input id="password" v-model="password" type="password" class="form-control" required />
+      <div class="invalid-feedback">Please provide a valid password.</div>
+    </div>
+    <button type="submit" class="btn btn-primary">Register</button>
+  </form>
+</template>
